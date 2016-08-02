0a2e20c54c9931a499d88acf2363748d9a6237fb
Garden <kernelgarden@gmail.com>
2016-08-02 19:06:42 +0900

    tree 수정

--- Ttree/app/assets/javascripts/tree.js
+++ Ttree/app/assets/javascripts/tree.js
@@ -1,3 +1,6 @@
+/*! (C) WebReflection Mit Style License */
+var CircularJSON=function(e,t){function l(e,t,o){var u=[],f=[e],l=[e],c=[o?n:"[Circular]"],h=e,p=1,d;return function(e,v){return t&&(v=t.call(this,e,v)),e!==""&&(h!==this&&(d=p-a.call(f,this)-1,p-=d,f.splice(p,f.length),u.splice(p-1,u.length),h=this),typeof v=="object"&&v?(a.call(f,v)<0&&f.push(h=v),p=f.length,d=a.call(l,v),d<0?(d=l.push(v)-1,o?(u.push((""+e).replace(s,r)),c[d]=n+u.join(n)):c[d]=c[0]):v=c[d]):typeof v=="string"&&o&&(v=v.replace(r,i).replace(n,r))),v}}function c(e,t){for(var r=0,i=t.length;r<i;e=e[t[r++].replace(o,n)]);return e}function h(e){return function(t,s){var o=typeof s=="string";return o&&s.charAt(0)===n?new f(s.slice(1)):(t===""&&(s=v(s,s,{})),o&&(s=s.replace(u,"$1"+n).replace(i,r)),e?e.call(this,t,s):s)}}function p(e,t,n){for(var r=0,i=t.length;r<i;r++)t[r]=v(e,t[r],n);return t}function d(e,t,n){for(var r in t)t.hasOwnProperty(r)&&(t[r]=v(e,t[r],n));return t}function v(e,t,r){return t instanceof Array?p(e,t,r):t instanceof f?t.length?r.hasOwnProperty(t)?r[t]:r[t]=c(e,t.split(n)):e:t instanceof Object?d(e,t,r):t}function m(t,n,r,i){return e.stringify(t,l(t,n,!i),r)}function g(t,n){return e.parse(t,h(n))}var n="~",r="\\x"+("0"+n.charCodeAt(0).toString(16)).slice(-2),i="\\"+r,s=new t(r,"g"),o=new t(i,"g"),u=new t("(?:^|([^\\\\]))"+i),a=[].indexOf||function(e){for(var t=this.length;t--&&this[t]!==e;);return t},f=String;return{stringify:m,parse:g}}(JSON,RegExp);
+
 /* 페이지 정보를 모델링한 데이터 구조 */
 function Page(title, url) {
   this.title = title;   // 해당 페이지의 title
@@ -6,6 +9,7 @@ function Page(title, url) {
 
 /* 브랜치 정보를 모델링한 데이터 구조 */
 function Branch(title) {
+  this.id = null;
   this.pages = [];
   this.title = title;
   this.parent = null;
@@ -160,7 +164,6 @@ Tree.prototype.remove = function(title, parentTitle, traversal) {
     if (index === undefined) {
       throw new Error('Node to remove does not exist.');
     } else {
-      console.log(parent.children[index]);
       parent.children[index].removeSubBranch(parent.children[index].traverseDF);
 
       childToRemove = parent.children.splice(index, 1);
@@ -171,6 +174,16 @@ Tree.prototype.remove = function(title, parentTitle, traversal) {
 
   return childToRemove;
 };
+
+Tree.prototype.first = function(search_title, traversal) {
+  var first = null;
+
+  var callback = function(branch) {
+    if (branch.title === search_title) {
+      first = branch;
+    }
+  }
+}
 
 /* test code */
 var tree = new Tree('one');
@@ -220,8 +233,6 @@ for (var i = 1; i < 100; i++) {
 
 //tree._root.children[0].removeSubBranch(tree._root.children[0].traverseDF);
 console.log(tree._root.children[0]);
-
-document.write("<script src='circular-json.js'></script>");
 
 var CircularJSON = window.CircularJSON;
 
