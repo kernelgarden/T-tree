ede3bc29e1e71cc9bc4e8db58dcb9102ab448960
Jiwon <win6ter@naver.com>
2016-08-04 12:54:34 +0000

    master

--- Ttree/app/controllers/users_controller.rb
+++ Ttree/app/controllers/users_controller.rb
@@ -2,10 +2,10 @@ class UsersController < ApplicationController
   def new
   end
   def index
-  	#@branches=Branch.arrange
-  	#render :json =>  Branch.json_tree(@branches)
-  	@works=Work.all
-  	render :json => @works
+  	@branches=Branch.arrange
+  	render :json =>  Branch.json_tree(@branches)
+  	#@works=Work.all
+  	#render :json => @works
   end
 
 
