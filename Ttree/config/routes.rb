0fc6de804082c37ecee843488d4c9ac7bb34b2ff
Jiwon <win6ter@naver.com>
2016-08-04 12:46:18 +0000

    master

--- Ttree/config/routes.rb
+++ Ttree/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers =>{:omniauth_callbacks =>"users/omniauth_callbacks" }
+  root 'main#home'
   get 'users/new'
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
