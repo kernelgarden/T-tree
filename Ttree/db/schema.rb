86e66b061d8a29e13c11a78637add67162dfedf0
SiHoon <shuni93@naver.com>
2016-09-07 06:45:28 +0000

    team

--- Ttree/db/schema.rb
+++ Ttree/db/schema.rb
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20160906122429) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "title"
+    t.text     "url"
     t.integer  "branch_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
