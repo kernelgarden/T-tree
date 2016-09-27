e3922ea442659236d0a9b35e23f08a33b9790fd9
Jiwon <win6ter@naver.com>
2016-09-27 20:54:19 +0000

    사이드바정렬까지완성

--- Ttree/app/models/unclassifiedpage.rb
+++ Ttree/app/models/unclassifiedpage.rb
@@ -10,7 +10,7 @@ class Unclassifiedpage < ApplicationRecord
   def self.json_time(nodes)
     nodes.map do |node|
         {:id => node.id, :title => node.title, :user_id => node.user_id, :url => node.url,
-         :created_at => node.created_at.strftime("%Y%m%e%H%M"), 
+         :created_at => node.created_at.strftime("%Y%m%e%H%M%S"), 
          :time => node.created_at.strftime("%Y.%m.%e %H:%M:%S %p")}
     end
     #attributes :id, :title, :user_id, :url, :created_at
