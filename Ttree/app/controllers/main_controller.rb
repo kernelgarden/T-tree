6a55619a0d1ac97ac602cddf8d65148684dc7d87
SiHoon <shuni93@naver.com>
2016-10-24 12:36:24 +0000

    header fix

--- Ttree/app/controllers/main_controller.rb
+++ Ttree/app/controllers/main_controller.rb
@@ -1,7 +1,7 @@
 class MainController < ApplicationController
 	skip_before_action :verify_authenticity_token
-	#before_action :logged_in_user, only: [:home]
-	#before_action :logged_in_user, only: [:login]
+	before_action :logged_in_user, only: [:home]
+	before_action :logged_in_user, only: [:login]
 	before_action :check_login, only: [:home]
 	before_action :check_authenticity, only: [:folderView]
 
